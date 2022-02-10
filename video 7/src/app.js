fd9ad82209656da872d9cbf6a32e4adc87f6af43
Ehtesham Ali <63139751+Ehteshamali-889@users.noreply.github.com>
2022-02-10 09:09:33 +0500

    Add files via upload

---
+++ video 7/src/app.js
@@ -0,0 +1,39 @@
+const path=require("path");
+const express=require("express");
+const app=express();
+const port=8000;
+
+// console.log(path.join(__dirname,"../public"));
+const staticpath=path.join(__dirname,"../public");
+const templatepath=path.join(__dirname,"../templates");
+
+// to set view engine
+app.set("view engine","hbs");
+app.set("views","templatepath");
+// built in middleware
+app.use(express.static(staticpath))
+
+// if found it will be displayed rest ignored
+app.get("/",(req,res)=>{
+    res.render("index",{
+        name: "Ehtesham Ali",
+    });
+})
+
+app.get("/",(req,res)=>{
+    res.write("<h1>Hello World</h1>");
+    res.send();
+})
+
+app.get("/temp",(req,res)=>{
+    res.send([
+        {
+            id:1,
+            name:"vinod",
+        }
+    ])
+})
+
+app.listen(port,()=>{
+    console.log(`AT ${port}`);
+})
